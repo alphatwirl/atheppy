e696932c9b8a00ce6d6ef8bb3e5a1e7f947da412
Tai Sakuma <tai.sakuma@gmail.com>
2018-11-25 14:28:42 +0000

    update ReadComponentConfig for python3

--- tests/heppyresult/test_ReadComponentConfig.py
+++ tests/heppyresult/test_ReadComponentConfig.py
@@ -1,35 +1,43 @@
-from atheppy.heppyresult import ReadComponentConfig
-import unittest
+# Tai Sakuma <tai.sakuma@gmail.com>
+import textwrap
 import io
 import os
 
+from atheppy.heppyresult import ReadComponentConfig
+
 ##__________________________________________________________________||
-sample_cmp_cfg = b"""MCComponent: QCD_HT_100To250_Chunk0
+sample_cmp_cfg = """MCComponent: QCD_HT_100To250_Chunk0
 	addWeight      :   1.0
 	efficiency     :   CFG: eff
 	triggers       :   []
 	xSection       :   28730000
 """
 
-##__________________________________________________________________||
-def mock_isfile(path): return False
+def io_from_txt(txt):
+    txt = textwrap.dedent(txt[1:])
+    try:
+        io_ = io.StringIO(txt)
+    except TypeError:
+        io_ = io.BytesIO(txt.encode())
+    return io_
+
+def mock_isfile(path):
+    return False
 
 ##__________________________________________________________________||
-class TestReadComponentConfig(unittest.TestCase):
-    def test_read(self):
-        readConfig = ReadComponentConfig()
-        file = io.BytesIO(sample_cmp_cfg)
-        expected = {b'addWeight': 1.0, b'efficiency': b'CFG: eff', b'triggers': [], b'xSection': 28730000}
-        self.assertEqual(expected, readConfig._readImp(file))
+def test_read():
+    readConfig = ReadComponentConfig()
+    file = io_from_txt(sample_cmp_cfg)
+    expected = {'addWeight': 1.0, 'efficiency': 'CFG: eff', 'triggers': [], 'xSection': 28730000}
+    assert expected == readConfig._readImp(file)
 
-    @unittest.skip("skip because of logging. assertLogs can be used here for Python 3.4")
-    def test_no_file(self):
-        isfile_org = os.path.isfile
-        os.path.isfile = mock_isfile
+def test_no_file():
+    isfile_org = os.path.isfile
+    os.path.isfile = mock_isfile
 
-        readConfig = ReadComponentConfig()
-        self.assertIsNone(readConfig('config.txt'))
+    readConfig = ReadComponentConfig()
+    assert readConfig('config.txt') is None
 
-        os.path.isfile = isfile_org
+    os.path.isfile = isfile_org
 
 ##__________________________________________________________________||
