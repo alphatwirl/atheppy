1b0545e55a3bb8591f17315235e8c0397d2932fa
Tai Sakuma <tai.sakuma@gmail.com>
2018-11-25 17:01:15 +0000

    update ReadCounter for python3

--- atheppy/heppyresult/ReadCounter.py
+++ atheppy/heppyresult/ReadCounter.py
@@ -27,7 +27,6 @@ class ReadCounter(object):
         # a line is written in the format '\t {level:<40} {count:>9} \t {eff1:4.2f} \t {eff2:6.4f}\n'
         # https://github.com/cms-sw/cmssw/blob/CMSSW_7_4_0/PhysicsTools/HeppyCore/python/statistics/counter.py
         exp = r'^\t (.*?) *([0-9e+-.]*) \t ([0-9e+-.]*) \t ([0-9e+-.]*)$'
-        exp = exp.encode()
         match = re.search(exp, line)
 
         if not match: return None, None
@@ -35,7 +34,6 @@ class ReadCounter(object):
         count = match.group(2)
         eff1 =  match.group(3)
         eff2 = match.group(4)
-        count = count.decode()
         return level, dict(count = ast.literal_eval(count), eff1 = float(eff1), eff2 = float(eff2))
 
 ##__________________________________________________________________||
--- tests/heppyresult/test_ReadCounter.py
+++ tests/heppyresult/test_ReadCounter.py
@@ -1,74 +1,83 @@
-from atheppy.heppyresult import ReadCounter
-import unittest
+# Tai Sakuma <tai.sakuma@gmail.com>
+import textwrap
 import collections
 import io
 import os
 
+from atheppy.heppyresult import ReadCounter
+
 ##__________________________________________________________________||
-sample_counts_txt = b"""Counter SkimReport :
+sample_counts_txt = """
+Counter SkimReport :
 	 All Events                                  500000 	 1.00 	 1.0000
 	 Sum Weights                              1042218.60703 	 2.08 	 2.0844
 
 
 """
 
-##__________________________________________________________________||
-def mock_isfile(path): return False
+def io_from_txt(txt):
+    txt = textwrap.dedent(txt[1:])
+    try:
+        io_ = io.StringIO(txt)
+    except TypeError:
+        io_ = io.BytesIO(txt.encode())
+    return io_
+
+def mock_isfile(path):
+    return False
 
 ##__________________________________________________________________||
-class TestReadCounter(unittest.TestCase):
+def test_read_file():
+    readCounter = ReadCounter()
+    file = io_from_txt(sample_counts_txt)
+    expected = collections.OrderedDict([('All Events', {'count': 500000.0, 'eff2': 1.0, 'eff1': 1.0}), ('Sum Weights', {'count': 1042218.60703, 'eff2': 2.0844, 'eff1': 2.08})])
+    assert expected == readCounter._readImp(file)
 
-    def test_read_file(self):
-        readCounter = ReadCounter()
-        file = io.BytesIO(sample_counts_txt)
-        expected = collections.OrderedDict([(b'All Events', {'count': 500000.0, 'eff2': 1.0, 'eff1': 1.0}), (b'Sum Weights', {'count': 1042218.60703, 'eff2': 2.0844, 'eff1': 2.08})])
-        self.assertEqual(expected, readCounter._readImp(file))
+def test_no_file():
+    isfile_org = os.path.isfile
+    os.path.isfile = mock_isfile
 
-    def test_no_file(self):
-        isfile_org = os.path.isfile
-        os.path.isfile = mock_isfile
+    readCounter = ReadCounter()
+    assert readCounter('SkimReport.txt') is None
 
-        readCounter = ReadCounter()
-        self.assertIsNone(readCounter('SkimReport.txt'))
+    os.path.isfile = isfile_org
 
-        os.path.isfile = isfile_org
+def test_readLine():
+    readCounter = ReadCounter()
 
-    def test_readLine(self):
-        readCounter = ReadCounter()
+    line = "	 Sum Weights                              1042218.60703 	 2.08 	 2.0844"
+    level, content = readCounter._readLine(line)
+    assert 'Sum Weights' == level
+    assert {'count': 1042218.60703, 'eff2': 2.0844, 'eff1': 2.08} == content
 
-        line = b"	 Sum Weights                              1042218.60703 	 2.08 	 2.0844"
-        level, content = readCounter._readLine(line)
-        self.assertEqual(b'Sum Weights', level)
-        self.assertEqual({'count': 1042218.60703, 'eff2': 2.0844, 'eff1': 2.08}, content)
+def test_readLine_tab_in_level():
+    readCounter = ReadCounter()
 
-    def test_readLine_tab_in_level(self):
-        readCounter = ReadCounter()
+    line = "	 Sum 	 Weights                              1042218.60703 	 2.08 	 2.0844"
+    level, content = readCounter._readLine(line)
+    assert 'Sum \t Weights' == level
+    assert {'count': 1042218.60703, 'eff2': 2.0844, 'eff1': 2.08} == content
 
-        line = b"	 Sum 	 Weights                              1042218.60703 	 2.08 	 2.0844"
-        level, content = readCounter._readLine(line)
-        self.assertEqual(b'Sum \t Weights', level)
-        self.assertEqual({'count': 1042218.60703, 'eff2': 2.0844, 'eff1': 2.08}, content)
+def test_readLine_tab_():
+    readCounter = ReadCounter()
 
-    def test_readLine_tab_(self):
-        readCounter = ReadCounter()
+    line = "\t Sum \t Weights                              1042218.60703 \t 2.08 \t 2.0844"
+    level, content = readCounter._readLine(line)
+    assert 'Sum \t Weights' == level
+    assert {'count': 1042218.60703, 'eff2': 2.0844, 'eff1': 2.08} == content
 
-        line = b"\t Sum \t Weights                              1042218.60703 \t 2.08 \t 2.0844"
-        level, content = readCounter._readLine(line)
-        self.assertEqual(b'Sum \t Weights', level)
-        self.assertEqual({'count': 1042218.60703, 'eff2': 2.0844, 'eff1': 2.08}, content)
+def test_readLine_scientific_notation():
+    readCounter = ReadCounter()
+    line = "	 Sum Weights                              3.73134089883e+12 	 154498.74 	 154498.7447"
+    level, content = readCounter._readLine(line)
+    assert 'Sum Weights' == level
+    assert {'count': 3.73134089883e+12, 'eff2': 154498.7447, 'eff1': 154498.74} == content
 
-    def test_readLine_scientific_notation(self):
-        readCounter = ReadCounter()
-        line = b"	 Sum Weights                              3.73134089883e+12 	 154498.74 	 154498.7447"
-        level, content = readCounter._readLine(line)
-        self.assertEqual(b'Sum Weights', level)
-        self.assertEqual({'count': 3.73134089883e+12, 'eff2': 154498.7447, 'eff1': 154498.74}, content)
-
-    def test_readLine_netative(self):
-        readCounter = ReadCounter()
-        line = b"	 too many objects after requirements         -848739 	 -1.00 	 -0.1717"
-        level, content = readCounter._readLine(line)
-        self.assertEqual(b'too many objects after requirements', level)
-        self.assertEqual({'count': -848739, 'eff1': -1.0, 'eff2': -0.1717}, content)
+def test_readLine_netative():
+    readCounter = ReadCounter()
+    line = "	 too many objects after requirements         -848739 	 -1.00 	 -0.1717"
+    level, content = readCounter._readLine(line)
+    assert 'too many objects after requirements' == level
+    assert {'count': -848739, 'eff1': -1.0, 'eff2': -0.1717} == content
 
 ##__________________________________________________________________||
